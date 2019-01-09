82f8e3657530679c20566ad033d422f83326ff9a
Dally Wally <dallin.watac.major@gmail.com>
2019-01-09 10:35:13 -0700

    added cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Page from './containers/page';
+import Navbar from './components/navbar';
+import Card from './components/card';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Page />
+        <Navbar />
+        <div className='container'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     );
   }
